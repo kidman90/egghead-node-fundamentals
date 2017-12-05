fdfe849a2393adbfb5f50aa878c3804d452ab0ac
kidman90 <kacprzak90@gmail.com>
2017-12-05 11:28:30 +0100

    01 Use the Node.js REPL shell

---
+++ 01.js
@@ -0,0 +1,14 @@
+1 + 2
+4 * 5
+var foo = 'name'
+var boo = 'other name'
+foo
+boo
+1 + 2 + 3
+_ + 4
+var myName = function () { return 'Will'; }
+myName()
+var sayMyName = function () {
+return 'Walter';
+}
+sayMyName()
