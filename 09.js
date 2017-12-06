4a0d4686c220fc81f3daa92b6e41a5d6320a1271
kidman90 <kacprzak90@gmail.com>
2017-12-06 14:26:50 +0100

    09 Create a TCP server using the Node.js Net Module

---
+++ 09.js
@@ -0,0 +1,16 @@
+// node 09.js
+// New-Object System.Net.Sockets.TcpClient('localhost', 3000)
+var net = require('net');
+var server = net.createServer(function (c) {
+  console.log('client connected');
+  c.on('data', function (d) {
+    console.log('data received: ' + d.toString());
+  });
+  c.on('end', function () {
+    console.log('client disconnected');
+  });
+});
+
+server.listen(3000, function () {
+  console.log('server started');
+});
